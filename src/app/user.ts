7f256b74ec339e7a21c0f7728ac7413de0688836
chad svastisalee <swasgrafx@gmail.com>
2021-07-12 13:46:53 -0500

    Implemented a user service

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,10 +1,10 @@
 export class User {
-    _id: string ='';
-    email: string='';
-    first_name: string='';
-    last_name: string='';
-    username: string='';
-    admin: boolean =false ;
+    _id: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+    username: string;
+    admin: boolean;
   }
   
   
