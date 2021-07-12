6937a43f8124de5f2ee9b1d8e96642f2c00e6261
chad svastisalee <swasgrafx@gmail.com>
2021-07-12 13:15:03 -0500

    Added a User schema

---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    _id: string ='';
+    email: string='';
+    first_name: string='';
+    last_name: string='';
+    username: string='';
+    admin: boolean =false ;
+  }
+  
+  
