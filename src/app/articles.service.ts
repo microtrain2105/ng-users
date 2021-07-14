1de674d13d3685ffa71dd9e3a95c91aa576c2a6a
chad svastisalee <swasgrafx@gmail.com>
2021-07-14 07:49:45 -0500

    Added the ability to edit an article

--- src/app/articles.service.ts
+++ src/app/articles.service.ts
@@ -22,7 +22,7 @@ export class ArticlesService {
   createArticle (article: Article): Observable<Article> {
     return this.http.post<Article>(this.url, article, httpOptions);
   }
-  editArticle(article: Article): Observable<Article> {
+    editArticle(article: Article): Observable<Article> {
     return this.http.put<Article>(this.url, article, httpOptions);
   }
 
