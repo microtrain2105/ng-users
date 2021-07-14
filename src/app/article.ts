016fc5806bba6baa9d5d8d2674f861d498898833
chad svastisalee <swasgrafx@gmail.com>
2021-07-14 07:25:22 -0500

    Create a Article Schema

---
+++ src/app/article.ts
@@ -0,0 +1,11 @@
+export class Article {
+    _id: String;
+    title: String;
+    slug: String;
+    description: String;
+    keywords: String;
+    body: String;
+    published: Date;
+    created: Date;
+    modified: Date;
+    }
